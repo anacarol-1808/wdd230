090e632e2ea6211926004e5bd0779336ab81046e
anacarol-1808 <anacarol_1808@hotmail.com>
2023-06-17 16:44:01 -0600

    my best version of darker mode

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,19 @@ hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("#main");
+let heading = document.querySelector("main-h2-heading");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🌙")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "✖";
+	} else {
+		main.style.background = "#fff";
+		main.style.color = "#000";
+		modeButton.textContent = "🌙";
+	}
+});
