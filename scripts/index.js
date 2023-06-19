1939ac29ab233017206f7ff38588e1614d0423de
anacarol-1808 <anacarol_1808@hotmail.com>
2023-06-18 19:31:50 -0600

    hamburguer and dark mode adjustments

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,23 @@
-const hamburguer = document.querySelector(".hamburguer");
-const navMenu = document.querySelector(".nav-menu");
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
 
-hamburguer.addEventListener("click", () => {
-    hamburguer.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
 
+/* ❔What does toggle mean?
+We could write separate add and remove statements. Toggle adds the class if it does not currently exist or removes the class if it does exist. 
+The CSS class rules will handle the different views, layouts, and displays.
+🗝️ JavaScript only applies the class value or not.
+*/
+
+
+
+
+/* Dark Mode setup */
 const modeButton = document.querySelector("#mode");
 const main = document.querySelector("#main");
 let heading = document.querySelector("main-h2-heading");
