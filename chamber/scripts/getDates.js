ed425a4177286a87f1b2beea1a3f34f883102705
anacarol-1808 <anacarol_1808@hotmail.com>
2023-06-18 19:02:17 -0600

    my best version for the hamburguer menu

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -5,11 +5,3 @@ document.getElementById("year").innerHTML = year;
 //Get the date and time the document was last modified
 let text = document.lastModified;
 document.getElementById("lastModified").innerHTML = text;
-
-const hamburguer = document.querySelector(".hamburguer");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburguer.addEventListener("click", () => {
-    hamburguer.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
