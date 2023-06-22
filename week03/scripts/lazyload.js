94ae0d350a8b3494aaffcf4db6d1c78dd190ebc7
anacarol-1808 <anacarol_1808@hotmail.com>
2023-06-22 09:19:27 -0600

    responsive image

---
+++ week03/scripts/lazyload.js
@@ -0,0 +1,7 @@
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("year").innerHTML = year;
+
+//Get the date and time the document was last modified
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = text;
