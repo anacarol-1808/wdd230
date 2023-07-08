d5c7657a93dbc22a52eea7da43dc13d9f725557e
anacarol-1808 <anacarol_1808@hotmail.com>
2023-07-08 10:31:45 -0600

    Fecth API

---
+++ week05/scripts/prophets.js
@@ -0,0 +1,45 @@
+/* https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json.*/
+
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+const displayProphets = (prophets) => {
+    // card build code goes here
+    prophets.forEach((prophet) => {
+        // Create elements to add to the div.cards element
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let birthDate = document.createElement('p');
+        let birthPlace = document.createElement('p');
+        let portrait = document.createElement('img');
+
+        // Build the h2 content out to show the prophet's full name
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        // Build the p content
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+        // Build the image portrait by setting all the relevant attributes
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        // Append the section(card) with the created elements
+        card.appendChild(fullName);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+    
+        cards.appendChild(card);
+    }); // end of arrow function and forEach loop
+}
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.prophets); // temporary testing of data retreival
+    displayProphets(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
+}
+
+getProphetData();
