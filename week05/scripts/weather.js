a97beaf62ed5a76d76ccf1075adb80710e3a3c5f
anacarol-1808 <anacarol_1808@hotmail.com>
2023-07-10 23:03:46 -0600

    my best version for monday morning

---
+++ week05/scripts/weather.js
@@ -0,0 +1,80 @@
+
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=22766f88ec197be24284d775f68e916c';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      // displayResults(data); // uncomment when ready
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${______}.___`;
+  let desc = data.weather[0].______;
+  weatherIcon.setAttribute('___', _____);
+  weatherIcon.setAttribute('___', _____);
+  captionDesc.textContent = `${desc}`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      //var parsedData = JSON.parse(data);
+      var stringOfData = JSON.stringify(data)
+      var objectOfData = JSON.parse(stringOfData)
+      console.log("Parsed data: " + objectOfData.coord); // testing only
+      displayResults(objectOfData); // uncomment when ready
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+function displayResults(data){
+    console.log("The data parameter values: " + data)
+    var parsedData = JSON.parse(data);
+    currentTemp.innerHTML = `${data.document} &deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data}.___`;
+    let desc = data.weather[0].______;
+    weatherIcon.setAttribute('___', _____);
+    weatherIcon.setAttribute('___', _____);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();*/
