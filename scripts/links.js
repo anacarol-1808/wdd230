ab25bf7dcbea6331573c0eb56ca9d16b4c6d25d6
anacarol-1808 <anacarol_1808@hotmail.com>
2023-07-12 08:21:30 -0600

    my best version for Wednesday

--- scripts/links.js
+++ scripts/links.js
@@ -5,24 +5,44 @@ const linksURL = "https://anacarol-1808.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
-    //displayLinks(data);
+    //console.log(data.weeks[0].links[0].url);
+    //console.log(data.weeks);
+    displayLinks(data.weeks);
 }
   
+const create = document.querySelector('#activity-list');
+
 getLinks();
 
-/*const list = document.querySelector('#activity-list');
+let index = 0;
 
-displayLinks(weeks) {
-    weeks.forEach(week => {
+function displayLinks(weeks) {
+    console.log(weeks.lenght)
+    weeks.forEach(item => {
+        // Create elements to add to the 'ul' activity-list element
         let line = document.createElement('li');
-        line.textContent = ${week.}
-
-        week.forEach(activity => {
-            let link = document.createElement('a');
-        })
+        let weekNumber = document.createElement('span');
         
+        // Build the 'span' content out to show the week number
+        weekNumber.textContent = `${item.week}: `;
+
+        // Append the ul(activity-list) with the created elements
+        line.appendChild(weekNumber);
+
+        // set attributes for anchor element
+        (item.links).forEach(element => {
+            console.log(element);
+            let anchor = document.createElement('a');
+            anchor.setAttribute('href', element.url);
+            anchor.textContent = `${element.title} | `;
+            line.appendChild(anchor);
+        });
 
         
+        
+        
+        create.appendChild(line);        
+        //console.log(item.links[0].url);
     });
-}*/
+    
+}
--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -38,7 +38,7 @@ const displayProphets = (prophets) => {
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    //console.table(data.prophets); // temporary testing of data retreival
+    console.table(data.prophets); // temporary testing of data retreival
     displayProphets(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
 }
 
