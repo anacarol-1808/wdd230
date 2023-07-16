01ae866b4e2459d4585a83b9b836d535430dd942
anacarol-1808 <anacarol_1808@hotmail.com>
2023-07-16 16:19:10 -0600

    weather adjustment

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -2,7 +2,7 @@
 // select HTML elements in the document
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+const captionDesc = document.querySelector('#weather-figcaption');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-23.6261&lon=-46.7917&units=imperial&appid=22766f88ec197be24284d775f68e916c';
 
