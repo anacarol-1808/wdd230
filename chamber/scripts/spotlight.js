c93c473d081208aa220c677c315c5eb68200ee11
anacarol-1808 <anacarol_1808@hotmail.com>
2023-07-18 12:53:02 -0600

    my best version

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -40,29 +40,26 @@ function displayMembers(data) {
         // console.log(numberRound);
         // console.log(data.members[0].membership);
 
-        if (data.members[numberRound].membership == 'gold' | data.members[numberRound].membership == 'silver') {
-            
-            arrayOfNumbers.forEach(item => {
-                if (item != numberRound) {
-                // Create needed elements
-                let heading = document.createElement('h3');
-                let paragraph = document.createElement('p');
+        let check = arrayOfNumbers.includes(numberRound, 0);
 
-                // Give content to the elements
-                heading.textContent = `${data.members[numberRound].name}`;
-                paragraph.textContent = `${data.members[numberRound].spotlight}`;
+        if (data.members[numberRound].membership == 'gold' | data.members[numberRound].membership == 'silver' && index == 0 && check ==  false) {
+            // Create needed elements
+            let heading = document.createElement('h3');
+            let paragraph = document.createElement('p');
 
-                //Append
-                division.appendChild(heading);
-                division.appendChild(paragraph);
+            // Give content to the elements
+            heading.textContent = `${data.members[numberRound].name}`;
+            paragraph.textContent = `${data.members[numberRound].spotlight}`;
 
-                index++;
-                arrayOfNumbers.push(numberRound);
-                //console.log(arrayOfNumbers);
-                } else {
-                    index = index;
-                }
-            });
+            //Append
+            division.appendChild(heading);
+            division.appendChild(paragraph);
+
+            index++;
+            arrayOfNumbers.push(numberRound);
+        }
+        else {
+            index = index;
         }
    
     }
