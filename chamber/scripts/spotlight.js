255111265c90ea1697985f6a96a73a58f255d379
anacarol-1808 <anacarol_1808@hotmail.com>
2023-07-17 23:49:21 -0600

    my best version

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -4,12 +4,47 @@
 
 
 const baseURL = "index.html";
-
 const linksURL = "data/members.json";
+
+const division = document.querySelector("#spotlightAdv");
 
 async function getMembers() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data.members[0].address);//testing only
+    console.log(data);//testing only
+    getLenght(data);
     displayMembers(data);
+}
+
+getMembers();
+
+let lenght = 0;
+
+function getLenght(data) {
+    (data.members).forEach(item => {
+        lenght++;
+    });
+    console.log(`This is lenght ${lenght}`);
+}   
+
+let index = 1;
+
+function displayMembers(data) {
+    //getting random position 
+    let number = Math.random() * (lenght-1);
+    let numberRound = Math.round(number);
+    console.log(numberRound);
+    console.log(data.members[0].membership);
+
+    if (data.members[numberRound].membership == 'gold' | data.members[numberRound].membership == 'silver') {
+        let heading = document.createElement('h3');
+        let paragraph = document.createElement('p');
+
+        heading.textContent = '${data.members[numberRound].name}';
+        
+    } else {
+        
+    }
+
+   
 }
